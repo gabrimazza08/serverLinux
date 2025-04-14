bf41184df168157f4caf348fecdb970b4bba7913
Gabriele <gmazza289@gmail.com>
2025-04-14 09:47:43 +0200

    esercizio strutture con coordinate punti

---
+++ 0414/punti.c
@@ -0,0 +1,43 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+struct punto {
+    float x;
+    float y;
+};
+
+// funzione per calcolare le distanze dei punti dall'origine
+void distanze(struct punto punti[]) {
+    for (int i = 0; i < N; i++) {
+        float distanzaAl2 = (punti[i].x)*(punti[i].x) + (punti[i].y)*(punti[i].y);
+        float distanzePunti[N];
+        float distanzePunti[i]=distanzaAl2[i];
+        printf("La distanza dall'origine al punto %d è: %.1f\n", i + 1, distanzaAl2);
+
+        printf("tutte le distanze: \n %.1f \n",distanzePunti[i]);
+    }
+}
+
+int distanzaMax(int distanzePunti, float distanzaAl2){
+    for (int i=0;i<N;i++){
+        int max=distanzaAl2;
+    }
+}
+
+int main() {
+    struct punto punti[N];
+    srand(time(0));
+
+    // genera casualmente i punti(range [-5.0; 5.0])
+    for (int i = 0; i < N; i++) {
+        punti[i].x = (rand() % 101 - 50) / 10.0;
+        punti[i].y = (rand() % 101 - 50) / 10.0;
+        printf("Il punto %d ha coordinate (%.1f, %.1f)\n", i + 1, punti[i].x, punti[i].y);
+    }
+
+    distanze(punti);
+
+    return 0;
+}
